a36f7cfd6903de1f496971fecd7c526c263ee171
Jackson Perez <toxicoboom7@gmail.com>
2024-06-23 18:34:41 -0400

    Update welcome.blade.php
    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,12 +93,28 @@
             <div class="w-full md:w-1/2 p-2 md:mr-10" data-aos="fade-right">
                 <!-- <h1 class="text-2xl tracking-wider play-medium-italic mb-4">Me</h1> -->
                 <p class="text-lg text-justify roboto-regular my-2">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio impedit reprehenderit, mollitia magnam sunt eos molestias quibusdam beatae, quod aliquam facere magni aspernatur quidem cum nemo aut? Eum, aspernatur voluptas.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, officiis beatae harum tenetur maiores quia ratione, ullam eos deserunt nobis, sequi excepturi molestias cupiditate a nisi necessitatibus architecto quae dolor.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tempora enim impedit error atque accusamus inventore numquam ab sequi, aspernatur ratione, dolor illo nostrum. Repellendus aliquam animi blanditiis! Doloribus, quidem?
+                Agroinsumos y Servicios ROA: Su aliado para el éxito en el campo
+                En Guasdualito, estado Apure, Agroinsumos y Servicios ROA se enorgullece de ser su proveedor integral de insumos agrícolas, asesoría técnica y servicios personalizados para el agro.
+                Con más de 6 años de experiencia, estamos comprometidos a ayudar a los productores de la región a maximizar su productividad y rentabilidad.
                 </p>
                 <p class="text-lg text-justify roboto-regular my-2">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, ab culpa ipsum consequatur distinctio, corrupti modi fugit voluptas nesciunt vero, id aut maxime eos? Repellendus autem reprehenderit dolorum culpa nihil!
+                Nuestra historia:
+
+                Nacimos en [año] con la visión de apoyar el desarrollo del sector agrícola en Guasdualito y sus alrededores. Desde entonces, hemos crecido de la mano de nuestros clientes, adaptándonos a sus necesidades y las exigencias del mercado. Hoy en día, somos una empresa sólida y reconocida por nuestra calidad, eficiencia y compromiso.
+
+                Misión, visión y valores:
+
+                Misión: Brindar a los productores agrícolas de Guasdualito y el estado Apure insumos de alta calidad, asesoría técnica experta y servicios personalizados que les permitan optimizar sus cultivos y alcanzar el éxito en su actividad.
+
+                Visión: Convertirnos en la empresa líder en insumos agrícolas y servicios agropecuarios en la región, reconocida por nuestra excelencia, innovación y compromiso con el desarrollo sostenible del campo.
+
+                Valores:
+
+                Responsabilidad: Trabajamos con ética y profesionalismo, asumiendo la responsabilidad de nuestros actos y compromisos.
+                Compromiso: Nos dedicamos con pasión a satisfacer las necesidades de nuestros clientes y contribuir al progreso del sector agrícola.
+                Calidad: Ofrecemos productos y servicios de la más alta calidad, garantizando su efectividad y seguridad.
+                Innovación: Buscamos constantemente nuevas soluciones y tecnologías para optimizar la productividad de nuestros clientes.
+                Sostenibilidad: Promovemos prácticas agrícolas sostenibles que cuiden el medio ambiente y aseguren la viabilidad del sector a largo plazo.
                 </p>
             </div>
         </div>
@@ -206,7 +222,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 512 512"><title>Instagram</title><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#000}</style><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
                 </a>
                 <a href="https://x.com/AgroISROA" class="hover:text-gray-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 512 512"><title>Twitter</title><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#000}</style><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 512 512"><title>X</title><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#000}</style><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
                 </a>
             </div>
             {{-- <div class="flex space-x-4">
